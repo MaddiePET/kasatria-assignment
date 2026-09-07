76fc47b76c3b0f51e71811faea822a62f2b014b3
MaddiePET <myatpaneithu.nge@gmail.com>
2026-09-07 23:57:07 +0800

    configuration strings for the sheet and cloud project client id

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  GOOGLE_CLIENT_ID: "",
+
+  SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR01rqmxkDjIflcZKkEIHBusUNGhYbBkzpXta46BAU6zozOK-eMxCtBLyiFLHVj0I1BNer_KiCgI6ZU/pub?gid=651668883&single=true&output=csv"
+};
