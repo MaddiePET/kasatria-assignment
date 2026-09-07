4e2c5a5b0bdaae5123c2d0eb221c91760098e049
MaddiePET <myatpaneithu.nge@gmail.com>
2026-09-08 00:10:45 +0800

    added google auth

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  GOOGLE_CLIENT_ID: "",
+  GOOGLE_CLIENT_ID: "932602496597-1ci12jb5c8h8j0v31knjfsiter0hm0i8.apps.googleusercontent.com",
 
   SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR01rqmxkDjIflcZKkEIHBusUNGhYbBkzpXta46BAU6zozOK-eMxCtBLyiFLHVj0I1BNer_KiCgI6ZU/pub?gid=651668883&single=true&output=csv"
 };
